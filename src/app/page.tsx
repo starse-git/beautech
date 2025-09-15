579957627edc98feaa730609178f3e808bf2504e
PPT <git.starse@gmail.com>
2025-09-15 19:10:17 +0900

    file upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ import FullImageSlider from "@/components/common/FullImageSlider";
 import SliderImage1 from "@/assets/img/home/slider_img01.jpg";
 import SliderImage2 from "@/assets/img/home/slider_img02.jpg";
 import SliderImage3 from "@/assets/img/home/slider_img03.jpg";
+import SliderImage1SP from "@/assets/img/home/slider_img01SP.jpg";
+import SliderImage2SP from "@/assets/img/home/slider_img02SP.jpg";
+import SliderImage3SP from "@/assets/img/home/slider_img03SP.jpg";
 export default function Home() {
   const images = [
     {
@@ -22,10 +25,29 @@ export default function Home() {
       alt: "SliderImage3",
     },
   ];
+  const imagesSP = [
+    {
+      src: SliderImage1SP,
+      alt: "SliderImage1",
+    },
+    {
+      src: SliderImage2SP,
+      alt: "SliderImage2",
+    },
+    {
+      src: SliderImage3SP,
+      alt: "SliderImage3",
+    },
+  ];
 
   return (
     <div className="w-full">
-      <FullImageSlider images={images}/>
+      <div className="hidden md:block">
+        <FullImageSlider images={images}/>
+      </div>
+      <div className="md:hidden">
+        <FullImageSlider images={imagesSP}/>
+      </div>
       <PublicLayout>
         <AboutSectionComponent />
         <NewsSectionComponent />

9fd2527a937b76d97eae18c3646e2495b2587e79
PPT <git.starse@gmail.com>
2025-09-16 10:13:40 +0900

    file upload

---
+++ src/app/about/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div className="w-full mt-[60px] md:mt-[100px]">
+      {/* MV */}
+      <div></div>
+      {/* Philosophy */}
+      <div></div>
+      {/* Message */}
+      <div></div>
+      {/* History */}
+      <div></div>
+      {/* Company */}
+      <div></div>
+      {/* Access */}
+      <div></div>
+    </div>
+  )
+}
+
+export default AboutPage;

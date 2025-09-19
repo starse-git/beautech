79009980afebbe5839aeedb5001e6d30f6312a2b
PPT <git.starse@gmail.com>
2025-09-19 10:46:58 +0900

    solve error

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import React from 'react'
 import SubBannerComponent from '@/components/common/SubBannerComponent';
 import Banner from '@/assets/img/business/business_banner.png';
